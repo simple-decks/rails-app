d777cb0a5b816dd647957d8397a7280a5c50597c
Anders Ramsay <andersr@gmail.com>
2013-12-12 15:44:30 -0500

    in progress

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -8,16 +8,25 @@ describe "Homepage" do
     end
 
     it "displays a presentation with one slide" do 
-
       expect(page).to have_selector("section")
     end
 
     it "displays one empty slide with default content" do
-
       expect(page).to have_content("New SimpleDeck")
     end
 
-    
+    it "allows users to input text when clicking on a slide"  do
+      click_link("edit_slide")
+      expect(page).to have_selector("textarea")
+    end
 
+    it "displays the content from the slide as the default content in the text area" do
+      @pres.slides[0].content = "Foo"
+      @pres.save
+      visit index_path
+      expect(page).to have_content("Foo")
+      click_link("edit_slide")
+      expect(page).to have_content("Foo")
+    end 
   end
 end
