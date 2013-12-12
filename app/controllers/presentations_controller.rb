87f0f793ee4ead931a9a0236da4b9112646f6f94
Anders Ramsay <andersr@gmail.com>
2013-12-12 16:09:16 -0500

    user sessions in progress

--- app/controllers/presentations_controller.rb
+++ app/controllers/presentations_controller.rb
@@ -1,8 +1,17 @@
 class PresentationsController < ApplicationController
-
-  def index
-    @presentation = Presentation.new
+   def index
+    @presentation = current_presentation
     @default_slide = @presentation.slides[0]
   end
-  
+
+  def current_presentation
+    if session[:presentation_id]
+      presentation = Presentation.find(session[:presentation_id])
+    else
+      presentation = Presentation.new
+      session[:presentation_id] = presentation.id
+    end
+      return presentation
+  end
+ 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131212164429) do
+ActiveRecord::Schema.define(version: 20131212205410) do
 
   create_table "presentations", force: true do |t|
     t.string   "author"
@@ -29,5 +29,12 @@ ActiveRecord::Schema.define(version: 20131212164429) do
   end
 
   add_index "slides", ["presentation_id"], name: "index_slides_on_presentation_id"
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
