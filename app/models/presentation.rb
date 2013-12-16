ab99d829188b9dbb9e77171b7f0849a24006276a
jnoconor <joe.oconor@gmail.com>
2013-12-16 18:35:59 -0500

    allow for separation of slides using --- as delimiter

--- app/models/presentation.rb
+++ app/models/presentation.rb
@@ -10,5 +10,20 @@
 #
 
 class Presentation < ActiveRecord::Base
+
+  after_create :content_html_wrapper
+
+  def content_html_wrapper
+    content = self.content
+    slide_array = content.split("\n---\n")
+    
+    html_content = []
+    slide_array.each_with_index do |content, index|
+      html_content << "<section id='#{index}'>#{content}</section>"
+    end
+
+    self.update(:content => html_content.join)
+  end
+
   
 end
--- spec/models/presentation_spec.rb
+++ spec/models/presentation_spec.rb
@@ -15,12 +15,22 @@ describe Presentation do
     
   context "presentation content" do
 
-    let (:presentation) {Presentation.new(:content => "slide content", :title => "my presentation")}
-
-    it "puts content in a single slide by default" do 
-      expect(presentation.content).to be("<section id='1'>slide content</section>")
+    before(:each) do 
+      Presentation.create(:content => "slide content", :title => "my presentation")
+      Presentation.create(:content => "first slide\n---\nthis is the next slide", :title => "multi-slide presentation")
     end
 
+    it "puts content in a single slide by default" do
+      pres = Presentation.find_by(:title => "my presentation")
+
+      expect(pres.content).to eq("<section id='0'>slide content</section>")
+    end
+
+    it "divides submitted content into slides based on the delimiter" do 
+      two_slide_presentation = Presentation.find_by(:title => "multi-slide presentation")
+
+      expect(two_slide_presentation.content).to eq("<section id='0'>first slide</section><section id='1'>this is the next slide</section>")
+    end
 
   end
 
