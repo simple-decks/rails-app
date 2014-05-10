343ee734719bf5fade8c9c99216eadcea7d46e17
Anders Ramsay <andersr@gmail.com>
2014-05-10 16:02:33 -0400

    override to_param with url_token

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,11 @@ RailsApp::Application.routes.draw do
   
   # root "presentations#index", :as => :index
 
-  get ":url_token/:presentation_title/edit", :as => :edit_token_url, to: 'presentations#edit'
+  get ":url_token/:url_title/edit", :as => :edit_token_url, to: 'presentations#edit'
+  # get ":url_token/*/edit", :as => :edit_token_url, to: 'presentations#edit'
 
-  get ":url_token/:presentation_title", :as => :token_url, to: 'presentations#show'
+  # get ":url_token/*", :as => :token_url, to: 'presentations#show'
+  get ":url_token/:url_title", :as => :token_url, to: 'presentations#show'
 
   resources :presentations do
     resources :slides
