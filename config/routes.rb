cc5e5facd21e91aa82457af085d4191cec810e78
Anders Ramsay <andersr@gmail.com>
2014-05-17 15:49:14 -0400

    routes file cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 RailsApp::Application.routes.draw do
   root "anonymous_slides#new"
 
-  get ":url_token/:url_title/edit", :as => :edit_token_url, to: 'presentations#edit'
-  get ":url_token/:url_title", :as => :token_url, to: 'presentations#show'
+  # get ":url_token/:url_title/edit", :as => :edit_token_url, to: 'presentations#edit'
+  # get ":url_token/:url_title", :as => :token_url, to: 'presentations#show'
 
   resources :presentations do
     resources :slides
