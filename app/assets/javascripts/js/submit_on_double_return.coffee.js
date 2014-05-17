8567cedb8921e34f85a8f6eab3df96fefe9687e2
Anders Ramsay <andersr@gmail.com>
2014-05-17 13:46:45 -0400

    ng new presentation on dbl return

--- app/assets/javascripts/js/submit_on_double_return.coffee.js
+++ app/assets/javascripts/js/submit_on_double_return.coffee.js
@@ -1,14 +1,11 @@
-// # $(document).ready ->
+// $(document).ready ->
+//   returns_qty = 0
 
-// #   returns_qty = 0
-  
-// #   $(".slide_content").keyup (e) ->
-// #     if e.keyCode == 13
-// #       returns_qty+= 1
-// #       if returns_qty == 2
-// #         returns_qty = 0
-// #         $("#new_anonymous_slide").submit()
-// #         alert "got two submits"
-// #         #$('input[type=submit]').closest("form").submit()
-// #     else
-// #       returns_qty = 0
+//   $(".submit-on-dbl-return").keyup (e) ->
+//     if e.keyCode == 13
+//       returns_qty+= 1
+//     if returns_qty == 2
+//       returns_qty = 0
+//       $('input[type=submit]').closest("form").submit()
+//     else
+//       returns_qty = 0
