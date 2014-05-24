6902a0c7e5c011eaa67b1dc46e98b715bef409a2
Anders Ramsay <andersr@gmail.com>
2014-05-24 12:15:35 -0400

    prep for ng routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 RailsApp::Application.routes.draw do
   root "anonymous_slides#new"
 
-  # get ":url_token/:url_title/edit", :as => :edit_token_url, to: 'presentations#edit'
-  # get ":url_token/:url_title", :as => :token_url, to: 'presentations#show'
+  # match "*path" => "anonymous_slides#new", via: [:get, :post]
+
+  get ":url_token/:url_title/edit", :as => :edit_token_url, to: 'presentations#edit'
+  get ":url_token/:url_title", :as => :token_url, to: 'presentations#show'
 
   resources :presentations do
     resources :slides
   end
+
+
 
 end
